6008526917afceb2840e5379616708bb1f355448
hebstr <ju.diez@protonmail.com>
2025-12-16 00:43:56 +0100

    .

--- scripts/_setup.R
+++ scripts/_setup.R
@@ -9,18 +9,24 @@ read_csv2("data/retinol.csv") |>
     bmi = round(bmi, 1),
     sexe =
       as_factor(sexe) |>
-        fct_recode("Masculin" = "1",
-                  "Féminin" = "2"),
+        fct_recode(
+          "Masculin" = "1",
+          "Féminin" = "2"
+        ),
     tabac =
       as_factor(tabac) |>
-        fct_recode("Jamais" = "1",
-                  "Sevré" = "2",
-                  "Actif" = "3"),
+        fct_recode(
+          "Jamais" = "1",
+          "Sevré" = "2",
+          "Actif" = "3"
+        ),
     vitamine =
       as_factor(vitamine) |>
-        fct_recode("Fréquent" = "1",
-                  "Rare" = "2",
-                  "Jamais" = "3") |>
+        fct_recode(
+          "Fréquent" = "1",
+          "Rare" = "2",
+          "Jamais" = "3"
+        ) |>
         fct_rev()
   ) |>
   set_variable_labels(
@@ -39,38 +45,39 @@ read_csv2("data/retinol.csv") |>
 
 set_opts(
   data = easy_descr(df),
-  acro =
-    acro(n ~ "nombre d'évènements",
-         N ~ "nombre d'observations",
-         BMI ~ "indice de masse corporelle",
-         AJ ~ "apport journalier"),
-  model =
-    lst(data = df[-c(20, 62, 171, 296), ],
-        data_glm =
-          mutate(.data = data,
-                 retplasma = ifelse(retplasma < median(retplasma), 0, 1)),
-        y = "retplasma",
-        x =
-          lst(uv = names(data),
-              mv =
-                p_picking(
-                  data = data,
-                  model = "lm",
-                  y = y,
-                  vars = uv,
-                  pv = set_opts()$pvalue$seuil)
-                ),
-        fun = reformulate(x$mv, y),
-        lm = lm(fun, data = data),
-        glm = glm(fun, family = binomial, data = data_glm)),
-  note =
-    list(n = \(x) {
+  acro = acro(
+    n ~ "nombre d'évènements",
+    N ~ "nombre d'observations",
+    BMI ~ "indice de masse corporelle",
+    AJ ~ "apport journalier"
+  ),
+  model = lst(
+    data = df[-c(20, 62, 171, 296), ],
+    data_glm = data |> mutate(retplasma = ifelse(retplasma < median(retplasma), 0, 1)),
+    y = "retplasma",
+    x = lst(
+      uv = names(data),
+      mv = p_picking(
+        data = data,
+        model = "lm",
+        y = y,
+        vars = uv,
+        pv = set_opts()$pvalue$seuil
+      )
+    ),
+    fun = reformulate(x$mv, y),
+    lm = lm(fun, data = data),
+    glm = glm(fun, family = binomial, data = data_glm)
+  ),
+  note = list(
+    n = \(x) {
       glue("{nrow(x)} observations, dont {sum(!complete.cases(x))}
            contenant au moins une valeur manquante.")
     },
     ajust =
       glue("Ajustement sur tous les facteurs statistiquement
-           significatifs dans l'analyse univariée."))
+           significatifs dans l'analyse univariée.")
+  )
 )
 
 ### AUTO EXEC ------------------------------------------------------------------
--- scripts/fig_dty.R
+++ scripts/fig_dty.R
@@ -1,38 +1,42 @@
-.stat <- \(stat, level) {
+.stat <- \(x) {
 
-  expr(do.call(!!enexpr(stat), list(get(.x)[sexe == levels(sexe)[!!enexpr(level)]])))
+  df |>
+    summarise(
+      median = median(.data[[x]], na.rm = TRUE),
+      mean = mean(.data[[x]], na.rm = TRUE),
+      .by = sexe
+    )
 
 }
 
 fig_dty <-
 opts$data$qt$vars$total |>
-  map(~ df |>
-        ggplot() +
-        aes(x = get(.x),
-            color = sexe,
-            fill = after_scale(color)) +
-        geom_density(alpha = 0.4,
-                     linewidth = 0.3) +
-        geom_rug(alpha = 0.5) +
-        geom_vline(aes(xintercept = !!.stat("median", 1)),
-                   color = opts$palette[1],
-                   linewidth = 0.5) +
-        geom_vline(aes(xintercept = !!.stat("mean", 1)),
-                   color = opts$palette[1],
-                   linewidth = 0.5,
-                   linetype = "dotted") +
-        geom_vline(aes(xintercept = !!.stat("median", 2)),
-                   color = opts$palette[2],
-                   linewidth = 0.5) +
-        geom_vline(aes(xintercept = !!.stat("mean", 2)),
-                   color = opts$palette[2],
-                   linewidth = 0.5,
-                   linetype = "dotted") +
-        labs(x = var_label(df[[.]]),
-             y = "Densité (%)") +
-        scale_color_manual(values = opts$palette) +
-        scale_y_continuous(labels = scales::label_percent(suffix = "")) +
-        theme_bar(grid = FALSE)) |>
+  map(
+    ~ df |>
+      ggplot() +
+      aes(
+        x = .data[[.]],
+        color = sexe,
+        fill = after_scale(color)
+      ) +
+      geom_density(alpha = 0.4, linewidth = 0.3) +
+      geom_rug(alpha = 0.5) +
+      geom_vline(
+        data = .stat(.),
+        mapping = aes(xintercept = median, color = sexe),
+        linewidth = 0.5
+      ) +
+      geom_vline(
+        data = .stat(.),
+        mapping = aes(xintercept = mean, color = sexe),
+        linewidth = 0.5,
+        linetype = "dotted"
+      ) +
+      labs(x = var_label(df[[.]]), y = "Densité (%)") +
+      scale_color_manual(values = opts$palette) +
+      scale_y_continuous(labels = scales::label_percent(suffix = "")) +
+      theme_bar(grid = FALSE)
+  ) |>
   wrap_plots() +
   plot_layout(axis_titles = "collect_y")
 
